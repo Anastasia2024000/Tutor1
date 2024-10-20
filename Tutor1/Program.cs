835871d11b6d7d77266027d9327dc5bb21cfb2d8
Anastasia Kuznetsova <www.Kuzya-Nastya@mail.ru>
2024-10-20 20:50:42 +0300

    fix

---
+++ Tutor1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Utilities;
+
+Console.WriteLine("Hello, World!");
+Class1.Print();
---
+++ Utilities/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace Utilities;
+
+public static class Class1
+{
+    public static void Print()
+    {
+        Console.WriteLine();
+
+    }
+
+    public static void PrintRandomNumber()
+    {
+        Random dice = new Random();
+        int roll = dice.Next(1, 7);
+        Console.WriteLine(roll);
+
+    }
+
+}
